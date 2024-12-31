d4a851270dce907475a5dde6eb29f61a1c81e3a7
kelly_k <pinkflamingo681@gmail.com>
2024-12-31 18:00:58 -0500

    initial commit and frame

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask("app")
 
 @app.route("/")
 def hello_world():
-  return "Hello, World!"
+  return render_template("villain.html")
 
 app.run(host='0.0.0.0', port=8080)
